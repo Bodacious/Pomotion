07c2409375c33025d057c25d52e9397d58165591
Bodacious <gavin@katanacode.com>
2014-09-01 20:31:45 +0100

    Added NSDate+SecsIn25Mins category

---
+++ vendor/NSDate+SecsIn25Mins/NSDate+SecsIn25Mins.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+      
+@interface NSDate (SecsIn25Mins)
+
++ (int) secsIn25Mins;
+
+@end
---
+++ vendor/NSDate+SecsIn25Mins/NSDate+SecsIn25Mins.m
@@ -0,0 +1,9 @@
+#import "NSDate+SecsIn25Mins.h"
+
+@implementation NSDate (SecsIn25Mins)
+
++ (int) secsIn25Mins {
+  return TARGET_IPHONE_SIMULATOR ? 10 : 1500;
+}
+
+@end
