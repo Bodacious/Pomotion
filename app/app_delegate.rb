8cd88f1cdd9e9cccef220f094006ad76dd22243e
Bodacious <gavin@katanacode.com>
2014-09-22 22:59:57 +0100

    Hello World Example

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,23 @@
 class AppDelegate
+  
+  def hello_world_label
+    @hello_world_label ||= begin
+      frame = CGRectMake(20,200,280,40)
+      label           = UILabel.alloc.initWithFrame(frame)
+      label.text      = "Hello world"
+      label.textColor = UIColor.whiteColor
+      label.textAlignment = UITextAlignmentCenter
+      label
+    end
+  end
+ 
+  def window
+    @window ||= UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+  end
+  
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    window.addSubview(hello_world_label)
+     window.makeKeyAndVisible    
     true
   end
 end
