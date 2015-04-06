07bbbe805837a6afa242b78858eed3273b69f1d7
Bodacious <gavin@katanacode.com>
2015-04-07 00:26:11 +0100

    Added task cell to tasks controller

--- app/controllers/tasks_view_controller.rb
+++ app/controllers/tasks_view_controller.rb
@@ -9,9 +9,22 @@ class TasksViewController < UITableViewController
   
   def add_button
     @add_button ||= UIBarButtonItem.alloc.
-    initWithBarButtonSystemItem(UIBarButtonSystemItemAdd, target: self, action: nil)
+    initWithBarButtonSystemItem(UIBarButtonSystemItemAdd, target: self, 
+      action: 'add_button_tapped:')
   end
  
+  def task_alert_view
+    @task_alert_view ||= UIAlertView.alloc.initWithTitle("Add A Task", 
+      message: "Insert the name of the task below",
+      delegate: self, cancelButtonTitle: "Add", otherButtonTitles: nil).tap do |alert|
+        alert.alertViewStyle = UIAlertViewStylePlainTextInput
+    end
+  end
+  
+  def add_button_tapped(sender)
+    task_alert_view.show
+  end
+  
   # = UITableViewDelegate =
  
   def tableView(table_view, heightForRowAtIndexPath: index_path)
@@ -28,7 +41,25 @@ class TasksViewController < UITableViewController
     [1, todays_tasks.count].max
   end
  
+  # = UIAlertViewDelegate =
+  
+  def alertView(alert_view, clickedButtonAtIndex: index_path)
+    text_field = alert_view.textFieldAtIndex(0)
+    if !text_field.text.to_s.empty?
+      create_new_task(name: text_field.text)
+      tableView.reloadData      
+      text_field.text = ''
+    end
+  end
+  
+ 
   private
+ 
+ 
+  def create_new_task(attributes)
+    Task.create(attributes)
+    Task.save
+  end
  
   def todays_tasks
     Task.all
