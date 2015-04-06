f58f7f9546eddfd499112f1cb7d3f1acc24899af
Bodacious <gavin@katanacode.com>
2015-04-07 00:12:27 +0100

    Added prompt message to EmptyCell

--- app/views/empty_cell.rb
+++ app/views/empty_cell.rb
@@ -1,3 +1,10 @@
 class EmptyCell < UITableViewCell
   
+  def initWithStyle(style, reuseIdentifier: reuseIdentifier)
+    super.tap do
+      self.styleClass = 'empty_cell'
+      textLabel.text = "Click '+' to add your first task"
+    end
+  end
+  
 end
