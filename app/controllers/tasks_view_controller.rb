8138107c47bc33d3a4074199c3ed53d871f0b46b
Bodacious <gavin@katanacode.com>
2015-04-07 00:11:11 +0100

    Added + button for new tasks

--- app/controllers/tasks_view_controller.rb
+++ app/controllers/tasks_view_controller.rb
@@ -1,3 +1,37 @@
 class TasksViewController < UITableViewController
   
+  def viewDidLoad
+    super
+    self.title = "Tasks"    
+    tableView.registerClass(EmptyCell, forCellReuseIdentifier: EmptyCell.name)
+    navigationItem.rightBarButtonItem = add_button    
+  end
+  
+  def add_button
+    @add_button ||= UIBarButtonItem.alloc.
+    initWithBarButtonSystemItem(UIBarButtonSystemItemAdd, target: self, action: nil)
+  end
+ 
+  # = UITableViewDelegate =
+ 
+  def tableView(table_view, heightForRowAtIndexPath: index_path)
+    todays_tasks.any? ? 75.0 : tableView.frame.size.height
+  end
+ 
+  # = UITableViewDataSource =
+ 
+  def tableView(table_view, cellForRowAtIndexPath: index_path)
+    table_view.dequeueReusableCellWithIdentifier(EmptyCell.name)
+  end
+ 
+  def tableView(table_view, numberOfRowsInSection: section)
+    [1, todays_tasks.count].max
+  end
+ 
+  private
+ 
+  def todays_tasks
+    Task.all
+  end
+  
 end
---
+++ app/views/empty_cell.rb
@@ -0,0 +1,3 @@
+class EmptyCell < UITableViewCell
+  
+end
